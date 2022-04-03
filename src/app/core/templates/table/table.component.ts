497390bea47342966781979a8bf2168e3845c289
alavrik <alavrik@emphasoft.com>
2022-04-03 14:22:36 +0700

    add rating

--- src/app/core/templates/table/table.component.ts
+++ src/app/core/templates/table/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Market, Odd } from '../../interfaces/bet365';
+import { Market } from '../../interfaces/bet365';
 
 @Component({
   selector: 'app-table',
@@ -8,12 +8,32 @@ import { Market, Odd } from '../../interfaces/bet365';
 })
 export class TableComponent implements OnInit {
   @Input() market: Market;
-  displayRows = ['time_str', 'ss', 'row1', 'row2', 'row3', 'rating'];
+  displayRows: string[];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    if (this.market.name.includes('1X2')) {
+      this.displayRows = ['game_time', 'ss', 'row1', 'row2', 'row3', 'world_time'];
+    } else {
+      this.displayRows = ['game_time', 'ss', 'row1', 'row2', 'row3', 'rating', 'world_time'];
+    }
+  }
+
+  getRatingColor(rating: number): string {
+    const signalRating = 0.6;
+    const green = rating > 0 ? 200 : 100;
+    const red = rating < 0 ? 200 : 100;
+    const ratio = Math.abs(rating / signalRating);
+    let opacity = 0;
+    if (ratio >= 1) {
+      opacity = 1;
+    }
+    if (ratio >= 0.5) {
+      opacity = 0.8 * (ratio - 0.4);
+    }
+    return `${red}, ${green}, 100, ${opacity}`;
   }
 
 }
